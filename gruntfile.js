e333f6b04b18e727eb861ef612f33cbee84d16ed
Julio Canares <juliocanares@gmail.com>
2014-11-16 05:02:01 -0800

    register and set by default concat and uglify

--- gruntfile.js
+++ gruntfile.js
@@ -32,9 +32,12 @@ module.exports = function (grunt) {
             }
         }
     });
-    
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('watched', ['watch']);
+
 };
