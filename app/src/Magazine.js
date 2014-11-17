b764d83134f01add55f27f2270bc5b00e07fce6a
Julio Canares <juliocanares@gmail.com>
2014-11-17 05:54:28 -0800

    initialize pixi.js

--- app/src/Magazine.js
+++ app/src/Magazine.js
@@ -6,6 +6,16 @@ var APP = APP || {};
 
 APP.Magazine = function () {
 
+    APP.Magazine.instance = this;
+
+    this.stage = new PIXI.Stage(0x000000);
+
+    this.renderer = PIXI.autoDetectRenderer(1024, 768);
+
+    document.body.appendChild(this.renderer.view);
+    this.renderer.view.style.position = "absolute";
+    this.renderer.view.style.top = "0";
+    this.renderer.view.style.left = "0";
 };
 
 APP.Magazine.prototype = Object.create(Object.prototype);
