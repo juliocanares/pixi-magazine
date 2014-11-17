2818f2f4b66e4f7d0f72167316d10b5c14d596f4
Julio Canares <juliocanares@gmail.com>
2014-11-17 05:48:25 -0800

    add magazine.js

---
+++ app/src/Magazine.js
@@ -0,0 +1,11 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+var APP = APP || {};
+
+APP.Magazine = function () {
+
+};
+
+APP.Magazine.prototype = Object.create(Object.prototype);
