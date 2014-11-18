0c1e1b0b2608201fbec1044550e45d7de726ef6c
Julio Canares <juliocanares@gmail.com>
2014-11-18 06:02:51 -0800

    add removeAllChildrens Util

---
+++ app/src/help/Util.js
@@ -0,0 +1,20 @@
+Util = function () {
+};
+
+Util.removeAllChildrens = function (f, d) {
+    for (var c = f.children.length - 1; c >= 0; c--) {
+        var a = true;
+        if (d != null) {
+            for (var b = 0; b < d.length; b++) {
+                if (f.getChildAt(c) == d[b]) {
+                    a = false
+                }
+            }
+        }
+        if (a) {
+            var e = f.getChildAt(c);
+            f.removeChild(e);
+            e = null
+        }
+    }
+};
