fe8b5d61be157a996bb1a1d3358c8a2bffb0d17a
Julio Canares <juliocanares@gmail.com>
2014-11-18 06:02:09 -0800

    add general values

---
+++ app/src/data/Values.js
@@ -0,0 +1,14 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+
+var APP = APP || {};
+
+APP.Values = function () {
+
+};
+
+
+APP.Values.pathImages = APP.Values.remoteServer + "img/";
