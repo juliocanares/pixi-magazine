aed06f0bb8997a2033760e7521604bec31ef9ee3
Julio Canares <juliocanares@gmail.com>
2014-11-18 06:05:58 -0800

    add postToTwitter share helper

--- app/src/help/Share.js
+++ app/src/help/Share.js
@@ -20,3 +20,9 @@ APP.Share.postToFacebook = function (a) {
 
     })
 };
+
+APP.Share.postToTwitter = function (a) {
+    var b = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(a.text);
+    a.link && (b += "&url=" + encodeURIComponent(a.link));
+    window.open(b, "_blank", "height=300,width=550,resizable=1");
+};
