07d126d9911de9b75f3625e3632a69968b3ff377
Julio Canares <juliocanares@gmail.com>
2014-11-18 06:05:43 -0800

    add post on Facebook gelper

---
+++ app/src/help/Share.js
@@ -0,0 +1,15 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+
+APP.Share = function () {
+};
+
+APP.Share.tweet = function () {
+
+};
+APP.Share.facebook = function () {
+
+};
