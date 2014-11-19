659a13349c2df134c826c6a66e88b7ef97afa47b
Julio Canares <juliocanares@gmail.com>
2014-11-19 06:14:58 -0800

    add fullscreen toogle

--- app/src/elements/FullScreenButton.js
+++ app/src/elements/FullScreenButton.js
@@ -22,3 +22,7 @@ APP.FullScreenButton = function (a, b, c, d) {
 
     }
 };
+
+APP.FullScreenButton.prototype.fullScreenToggle = function () {
+    this.f ? document.mozCancelFullScreen ? document.mozCancelFullScreen() : document.webkitCancelFullScreen ? document.webkitCancelFullScreen() : document.cancelFullScreen() : document.body.mozRequestFullScreen ? document.body.mozRequestFullScreen() : document.body.webkitRequestFullScreen && document.body.webkitRequestFullScreen()
+};
