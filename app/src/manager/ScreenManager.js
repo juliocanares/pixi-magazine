d65ef5e387485b5955537feceb32fc434d0e100f
Julio Canares <juliocanares@gmail.com>
2014-11-19 06:16:33 -0800

    initialize some properties

--- app/src/manager/ScreenManager.js
+++ app/src/manager/ScreenManager.js
@@ -10,7 +10,15 @@ APP.ScreenManager = function () {
     this.screenContainer = new PIXI.DisplayObjectContainer();
     this.addChild(this.screenContainer);
 
+    this._currentScreenID = 0;
 
+    this.currentScreen = null;
+
+    this.count = 0;
+
+    this.screens = [APP.Page1 , APP.Page2, APP.Page3, APP.Page4, APP.Page5 , APP.Page6, APP.Page7 , APP.Page8, APP.Page9, APP.Page10, APP.Page11];
+
+    this.currentScreenID = 0;
 };
 APP.ScreenManager.prototype = Object.create(PIXI.DisplayObjectContainer.prototype);
 
