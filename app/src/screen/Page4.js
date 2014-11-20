16406395736ea2067957a7eb2f5d3a5c21409945
Julio Canares <juliocanares@gmail.com>
2014-11-20 06:27:00 -0800

    add animations for page4

--- app/src/screen/Page4.js
+++ app/src/screen/Page4.js
@@ -54,3 +54,23 @@ APP.Page4.prototype.buildView = function () {
 
     this.animIn();
 };
+
+APP.Page4.prototype.animIn = function () {
+    this.imageFadeInOut(this.image01, $.proxy(function () {
+        this.imageFadeInOut(this.image02, $.proxy(function () {
+            TweenMax.to(this.image03, 1, {alpha: 1, ease: Cubic.easeInOut, onComplete: $.proxy(function () {
+                TweenMax.to(this.image03.position, 1, {delay: 1, y: "-=100", ease: Cubic.easeInOut});
+                TweenMax.to(this.image04, 1, {delay: 1, alpha: 1});
+                TweenMax.to(this.image04.position, 1, {delay: 1, y: stageHBase - (this.image04.height + 40), ease: Cubic.easeInOut});
+                TweenMax.to(this.image05, 1, {delay: 1.3, alpha: 1});
+                TweenMax.to(this.image06, 1, {delay: 1.3, alpha: 1});
+            }, this)});
+        }, this));
+    }, this));
+};
+
+APP.Page4.prototype.imageFadeInOut = function (target, complete) {
+    TweenMax.to(target, 1, {alpha: 1, ease: Cubic.easeInOut, onComplete: function () {
+        TweenMax.to(target, 1, {delay: 2, alpha: 0, ease: Cubic.easeOut, onComplete: complete})
+    }});
+};
