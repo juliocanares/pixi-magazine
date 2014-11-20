4fcaf2d4773518902d8b1711e9ade99ace48a3b3
Julio Canares <juliocanares@gmail.com>
2014-11-20 06:22:11 -0800

    initialize buttons

--- app/src/manager/ArrowsManager.js
+++ app/src/manager/ArrowsManager.js
@@ -4,7 +4,18 @@
  */
 
 APP.ArrowsManager = function () {
-   
+    PIXI.DisplayObjectContainer.call(this);
+
+    this.leftArrow = PIXI.Sprite.fromImage(APP.Values.pathImages + "/ui/leftArrow.png");
+    this.rightArrow = PIXI.Sprite.fromImage(APP.Values.pathImages + "/ui/rightArrow.png");
+
+    this.leftArrow.anchor = this.rightArrow.anchor = {x: .5, y: .5};
+
+    this.leftArrow.interactive = this.rightArrow.interactive = true;
+
+    this.leftArrow.buttonMode = this.rightArrow.buttonMode = true;
+    this.addChild(this.leftArrow);
+    this.addChild(this.rightArrow);
 };
 
 APP.ArrowsManager.prototype = Object.create(PIXI.DisplayObjectContainer.prototype);
