20e440d523f704f74659ddb28631fb3fca272331
Julio Canares <juliocanares@gmail.com>
2014-11-20 06:26:32 -0800

    add page 4

---
+++ app/src/screen/Page4.js
@@ -0,0 +1,13 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+var APP = APP || {};
+
+APP.Page4 = function () {
+    APP.BasePage.call(this, 'page4');
+};
+
+APP.Page4.constructor = APP.Page4;
+APP.Page4.prototype = Object.create(APP.BasePage.prototype);
