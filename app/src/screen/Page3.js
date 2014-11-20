1e749cf9edc96b4b15307fdf0f2ad58e7532e92f
Julio Canares <juliocanares@gmail.com>
2014-11-20 06:25:45 -0800

    add page 3

---
+++ app/src/screen/Page3.js
@@ -0,0 +1,13 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+var APP = APP || {};
+
+APP.Page3 = function () {
+    APP.BasePage.call(this, 'page3');
+};
+
+APP.Page3.constructor = APP.Page3;
+APP.Page3.prototype = Object.create(APP.BasePage.prototype);
