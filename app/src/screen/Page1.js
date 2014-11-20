ba4cd3f5150d57c9071dace4f65b8e750c7d71b4
Julio Canares <juliocanares@gmail.com>
2014-11-20 06:23:02 -0800

    add page1 extending from basepage

---
+++ app/src/screen/Page1.js
@@ -0,0 +1,13 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+var APP = APP || {};
+
+APP.Page1 = function () {
+    APP.BasePage.call(this, 'page1');
+};
+
+APP.Page1.constructor = APP.Page1;
+APP.Page1.prototype = Object.create(APP.BasePage.prototype);
