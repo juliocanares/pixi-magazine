be2a6ef711796b58560a6c705ad532b41f0f9f5a
Julio Canares <juliocanares@gmail.com>
2014-11-20 06:24:08 -0800

    add page2

---
+++ app/src/screen/Page2.js
@@ -0,0 +1,13 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+var APP = APP || {};
+
+APP.Page2 = function () {
+    APP.BasePage.call(this, 'page2');
+};
+
+APP.Page2.constructor = APP.Page2;
+APP.Page2.prototype = Object.create(APP.BasePage.prototype);
