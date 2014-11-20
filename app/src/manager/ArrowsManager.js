99a2c5826f0e439ff88a1408d3d46f70c6c810a2
Julio Canares <juliocanares@gmail.com>
2014-11-20 06:22:01 -0800

    add arrowmanager

---
+++ app/src/manager/ArrowsManager.js
@@ -0,0 +1,10 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+APP.ArrowsManager = function () {
+   
+};
+
+APP.ArrowsManager.prototype = Object.create(PIXI.DisplayObjectContainer.prototype);
