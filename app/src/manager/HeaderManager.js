637419e9635e02e2eaa178831fcb7994117441cb
Julio Canares <juliocanares@gmail.com>
2014-11-20 06:20:43 -0800

    add headermanager

---
+++ app/src/manager/HeaderManager.js
@@ -0,0 +1,14 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+var APP = APP || {};
+
+APP.HeaderManager = function () {
+    PIXI.DisplayObjectContainer.call(this);
+
+
+};
+
+APP.HeaderManager.constructor = APP.HeaderManager;
+APP.HeaderManager.prototype = Object.create(PIXI.DisplayObjectContainer.prototype);
