a18f883f2556d8ba5a5fb2c8bfd07f04ff04ad2e
Julio Canares <juliocanares@gmail.com>
2014-11-20 06:21:42 -0800

    add boilerplate code to footermanager

---
+++ app/src/manager/FooterManager.js
@@ -0,0 +1,15 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+var APP = APP || {};
+
+APP.FooterManager = function () {
+    PIXI.DisplayObjectContainer.call(this);
+};
+
+APP.FooterManager.constructor = APP.FooterManager;
+APP.FooterManager.prototype = Object.create(PIXI.DisplayObjectContainer.prototype);
+
+APP.FooterManager.prototype.resize = function () {
+};
