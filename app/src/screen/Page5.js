15eac3f218b02395635a152ef7e2ae1eaafe4292
Julio Canares <juliocanares@gmail.com>
2014-11-21 06:29:00 -0800

    add page 5

---
+++ app/src/screen/Page5.js
@@ -0,0 +1,13 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+var APP = APP || {};
+
+APP.Page5 = function () {
+    APP.BasePage.call(this, 'page5');
+};
+
+APP.Page5.constructor = APP.Page5;
+APP.Page5.prototype = Object.create(APP.BasePage.prototype);
