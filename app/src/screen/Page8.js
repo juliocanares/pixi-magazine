d93a9b291f1d548def452a3a0bf2e38e2cd58f0f
Julio Canares <juliocanares@gmail.com>
2014-11-21 06:31:14 -0800

    add page 8 view

---
+++ app/src/screen/Page8.js
@@ -0,0 +1,13 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+var APP = APP || {};
+
+APP.Page8 = function () {
+    APP.BasePage.call(this, 'page8');
+};
+
+APP.Page8.constructor = APP.Page8;
+APP.Page8.prototype = Object.create(APP.BasePage.prototype);
