f838927a9b74d8fe23e7a4b6cd8ae6468cb18e8c
Julio Canares <juliocanares@gmail.com>
2014-11-21 06:30:36 -0800

    add page 7

---
+++ app/src/screen/Page7.js
@@ -0,0 +1,18 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+var APP = APP || {};
+
+APP.Page7 = function () {
+    APP.BasePage.call(this, 'page7');
+};
+
+APP.Page7.constructor = APP.Page7;
+APP.Page7.prototype = Object.create(APP.BasePage.prototype);
