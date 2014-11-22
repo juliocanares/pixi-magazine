f2ef43238125799a9d6995c9e7a63a483326ff0e
Julio Canares <juliocanares@gmail.com>
2014-11-22 06:37:22 -0800

    initialize and add to stage the managers

--- app/src/Magazine.js
+++ app/src/Magazine.js
@@ -18,9 +18,27 @@ APP.Magazine = function () {
     this.renderer.view.style.left = "0";
 
     new APP.SceneModel();
+
+    this.headerManager = new APP.HeaderManager();
+
+    this.arrowsManager = new APP.ArrowsManager();
+
+    this.screenManager = new APP.ScreenManager();
+
+    this.footerManager = new APP.FooterManager();
+
+    this.stage.addChild(this.headerManager);
+    this.stage.addChild(this.screenManager);
+    this.stage.addChild(this.arrowsManager);
+    this.stage.addChild(this.footerManager);
+
+ 
+
+    requestAnimationFrame(this.update.bind(this));
 };
 
 APP.Magazine.prototype = Object.create(Object.prototype);
+
 
 
 APP.Magazine.prototype.update = function () {
