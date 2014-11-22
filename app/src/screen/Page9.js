069bd7f35ec802cca7c73c6cab521c6508487358
Julio Canares <juliocanares@gmail.com>
2014-11-22 06:33:25 -0800

    add page 9

---
+++ app/src/screen/Page9.js
@@ -0,0 +1,18 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+var APP = APP || {};
+
+APP.Page9 = function () {
+    APP.BasePage.call(this, 'page9');
+};
+
+APP.Page9.constructor = APP.Page9;
+APP.Page9.prototype = Object.create(APP.BasePage.prototype);
