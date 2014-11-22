0becc7f1712b2852d475bf77864f19c9a4f82d57
Julio Canares <juliocanares@gmail.com>
2014-11-22 06:34:22 -0800

    add page10

---
+++ app/src/screen/Page10.js
@@ -0,0 +1,13 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+var APP = APP || {};
+
+APP.Page10 = function () {
+    APP.BasePage.call(this, 'page10');
+};
+
+APP.Page10.constructor = APP.Page10;
+APP.Page10.prototype = Object.create(APP.BasePage.prototype);
